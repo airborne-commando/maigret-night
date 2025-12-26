e03bd24caef28f96c70c5c776b520e5fabf25d21
Nthompson096 <nxthompson96@gmail.com>
2025-12-26 02:41:29 -0500

    Cleaner code, multi line probably will not work; use a text editor like sublime or geany and set the text.txt syntax to python

--- maigret_mods/filter_parser.py
+++ maigret_mods/filter_parser.py
@@ -3,20 +3,17 @@
 Filter Parser for Blackbird OSINT tool with comment support
 Supports:
 - # as single-line comments
-- /* ... */ as multi-line comment blocks
-- Filter validation and cleaning
+- Simple and efficient parsing
 """
 
 import re
 import os
 
 class FilterParser:
-    """Parser for filter files with comment support"""
+    """Parser for filter files with # comment support"""
     
     def __init__(self):
         self.single_line_comment = re.compile(r'^\s*#.*$')
-        self.multi_line_start = re.compile(r'/\*')
-        self.multi_line_end = re.compile(r'\*/')
     
     def parse_filter_file(self, file_path):
         """
@@ -38,38 +35,31 @@ class FilterParser:
     
     def parse_filter_content(self, content):
         """
-        Parse filter content string and remove comments
+        Parse filter content string and remove # comments
         
         Args:
-            content (str): Filter content with comments
+            content (str): Filter content with # comments
         
         Returns:
             list: Clean filter strings
         """
         lines = content.split('\n')
         clean_lines = []
-        in_comment_block = False
         
         for line in lines:
-            # Remove inline comments
-            line = self._remove_inline_comments(line, in_comment_block)
+            # Remove inline # comments
+            line = self._remove_inline_comments(line)
             
-            # Check for single-line comment
-            if self.single_line_comment.match(line):
-                continue
-            
-            # Check for multi-line comment blocks
-            line, in_comment_block = self._process_multi_line_comments(line, in_comment_block)
-            
-            # Skip empty lines and comment-only lines
-            if line.strip() and not in_comment_block:
-                clean_lines.append(line.strip())
+            # Skip if whole line is a comment or empty
+            line = line.strip()
+            if line and not self.single_line_comment.match(line):
+                clean_lines.append(line)
         
         return clean_lines
     
-    def _remove_inline_comments(self, line, in_comment_block):
-        """Remove inline # comments when not in a block comment"""
-        if not in_comment_block and '#' in line:
+    def _remove_inline_comments(self, line):
+        """Remove inline # comments"""
+        if '#' in line:
             # Find the first # that's not inside quotes
             in_quotes = False
             quote_char = None
@@ -84,35 +74,6 @@ class FilterParser:
                 elif char == '#' and not in_quotes:
                     return line[:i].rstrip()
         return line
-    
-    def _process_multi_line_comments(self, line, in_comment_block):
-        """Process /* ... */ comment blocks"""
-        result_line = ""
-        i = 0
-        
-        while i < len(line):
-            if in_comment_block:
-                # Look for end of comment block
-                end_match = self.multi_line_end.search(line[i:])
-                if end_match:
-                    i += end_match.end()  # Skip past */
-                    in_comment_block = False
-                else:
-                    i = len(line)  # Skip rest of line
-            else:
-                # Look for start of comment block
-                start_match = self.multi_line_start.search(line[i:])
-                if start_match:
-                    # Add text before comment
-                    result_line += line[i:start_match.start() + i]
-                    i += start_match.end()  # Skip past /*
-                    in_comment_block = True
-                else:
-                    # No comment block, add remaining text
-                    result_line += line[i:]
-                    break
-        
-        return result_line, in_comment_block
     
     def validate_filter(self, filter_string):
         """
@@ -140,49 +101,53 @@ class FilterParser:
                 return False
         
         return True
+
+    def validate_filters(self, filters):
+        """
+        Validate a list of filter strings
+        
+        Args:
+            filters (list): List of filter strings
+        
+        Returns:
+            tuple: (valid_filters, invalid_filters)
+        """
+        valid_filters = []
+        invalid_filters = []
+        
+        for filter_str in filters:
+            if self.validate_filter(filter_str):
+                valid_filters.append(filter_str)
+            else:
+                invalid_filters.append(filter_str)
+        
+        return valid_filters, invalid_filters
     
     def create_example_filter_file(self, file_path):
         """
-        Create an example filter file with comments
+        Create an example filter file with # comments
         
         Args:
             file_path (str): Path to create example file
         """
-        example_content = """# Example Blackbird Filter File with Comments
+        example_content = """# Example Blackbird Filter File with # Comments
 # ============================================
 # Use # for single-line comments
-# Use /* ... */ for comment blocks
 
 # Some users internet footprint - These sites will be excluded
 uri_check!=https://api.tracker.gg/api/v2/apex/standard/profile/origin/{account}
 uri_check!=https://gitlab.archlinux.org/api/v4/users?username={account}
 uri_check!=https://archiveofourown.org/users/{account}
 
-# Sites to comment out for now (using single-line comments)
+# Sites to comment out for now
 # uri_check!=https://some.site/{account}
 name!=weebly
 name!=Etoro
 name!=Lemon8
-# name!=gumroad  # Temporarily disabled
+# name!=gumroad  # Temporarily disabled (inline comment)
 name!=Bandcamp
 
-/*
-Will check these sites later - This is a comment block
-Everything between /* and */ is ignored
-Useful for disabling large sections temporarily
-*/
-
-uri_check!=https://hometech.social/api/v1/accounts/lookup?acct={account}
-uri_check!=https://hostux.social/api/v1/accounts/lookup?acct={account}
-
-/*
-Another comment block - You can nest single-line comments inside:
-# This single-line comment is also ignored inside the block
-uri_check!=https://independent.academia.edu/{account}
-uri_check!=https://imginn.com/{account}/
-*/
-
-# Back to active filters - Mixed with comments
+# Multiple filters can be combined
 name~twitter  # This finds sites with "twitter" in the name
 cat=social and e_code=200  # Only social sites that return 200
 
@@ -210,13 +175,4 @@ def parse_filters_from_string(filter_string):
 
 def validate_filters(filters):
     """Validate a list of filter strings"""
-    valid_filters = []
-    invalid_filters = []
-    
-    for filter_str in filters:
-        if parser.validate_filter(filter_str):
-            valid_filters.append(filter_str)
-        else:
-            invalid_filters.append(filter_str)
-    
-    return valid_filters, invalid_filters
+    return parser.validate_filters(filters)
